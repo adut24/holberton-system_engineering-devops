e640b5a3c15cd5eef6c23adfc36b88bc153ff5c9
adut24 <dutertre.alexandre@laposte.net>
2022-08-16 17:29:16 +0000

    Install and configure Nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,16 @@
+# Install and configure Nginx
+package {'nginx':
+  install_options => '-y',
+}
+
+file {'Configure the home page':
+  ensure    => 'present',
+  path      => '/var/www/html/index.nginx-debian.html',
+  content   => 'Hellow World'
+}
+
+file_line {'Redirection':
+  path  => '/etc/nginx/sites-available/default',
+  after => 'server_name _;',
+  line  => 'location /redirect_me { rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent; }',
+}
