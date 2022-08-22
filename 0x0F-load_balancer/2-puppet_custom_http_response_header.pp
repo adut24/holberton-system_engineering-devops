08b645311327f0d58d8095d90cb7a52104f6b87c
adut24 <dutertre.alexandre@laposte.net>
2022-08-22 10:39:35 +0000

    Configure a server with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,50 @@
+# Install and configure Nginx
+package {'nginx':
+  ensure   => 'installed',
+  provider => 'apt',
+}
+
+file {'Configure the home page':
+  ensure  => 'present',
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World'
+}
+
+file {'Configure the page for the error 404':
+  ensure  => 'present',
+  path    => '/var/www/html/404.html',
+  content => 'Ceci n\'est pas une page',
+}
+
+file_line {'Redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'location /redirect_me { rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent; }',
+}
+
+file_line {'error page':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'error_page 404 /404.html;',
+}
+
+file_line {'error page redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'error_page 404 /404.html;',
+  line   => 'location /404 { root /var/www/html; internal; }',
+}
+
+file_line {'Custom header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'add_header X-Served-By $hostname;',
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  require => Package['nginx'],
+}
