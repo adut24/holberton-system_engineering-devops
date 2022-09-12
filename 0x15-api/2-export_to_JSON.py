56e11386c1bd5c6165ff9c322fa38b0490ef810c
adut24 <dutertre.alexandre@laposte.net>
2022-09-12 11:57:19 +0200

    Use the json module

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """Export data to a JSON file"""
+import json
 import requests
 from sys import argv
 
 
 if __name__ == '__main__':
-    json_dict = {}
     task_list = []
     url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
     r = requests.get(url).json()
     url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])
     todos = requests.get(url).json()
     for task in todos:
-        dict = {}
-        dict.update({"task": task.get('title'),
-                     "completed": task.get('completed'),
-                     "username": r.get('username')})
-        task_list.append(dict)
-    json_dict.update({"{}".format(argv[1]): task_list})
+        task.pop('id')
+        task.pop('userId')
+        task['task'] = task.pop('title')
+        task.update({'username': r.get('username')})
+        task_list.append(task)
+    dict = {'{}'.format(argv[1]): task_list}
     with open('{}.json'.format(argv[1]), 'w') as f:
-        f.write(str(json_dict))
+        f.write(json.dumps(dict))
