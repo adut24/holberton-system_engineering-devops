11e9713027ed4c1233cbacd6a18f3599da80d793
adut24 <dutertre.alexandre@laposte.net>
2022-09-12 11:21:37 +0200

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Export data to a JSON file"""
+import requests
+from sys import argv
+import json
+
+
+if __name__ == '__main__':
+    json_dict = {}
+    task_list = []
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    r = requests.get(url).json()
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])
+    todos = requests.get(url).json()
+    for task in todos:
+        dict = {}
+        dict.update({"task": task.get('title'),
+                     "completed": task.get('completed'),
+                     "username": r.get('username')})
+        task_list.append(dict)
+    json_dict.update({"{}".format(argv[1]): task_list})
+    with open('{}.json'.format(argv[1]), 'w') as f:
+        f.write(str(json_dict))
