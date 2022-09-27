52a9a5219849f596e5781ca54180bbe4172bd2cb
adut24 <dutertre.alexandre@laposte.net>
2022-09-27 12:13:59 +0200

    Display the number of subscribers of a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module looking for the number of subscribers to a subreddit"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Search the number of subscribers to a subreddit"""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    user = {'User-Agent': 'Test123'}
+    response = requests.get(url, headers=user, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    return response.json().get('data').get('subscribers')
