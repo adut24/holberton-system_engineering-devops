660e391bdd33cb2d1614a0a17b30416e2c7564e5
adut24 <dutertre.alexandre@laposte.net>
2022-09-27 14:15:31 +0200

    Correction of base case

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,12 +8,12 @@ def recurse(subreddit, hot_list=[], after=''):
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?after={after}'
     user = {'User-Agent': 'Test123'}
     response = requests.get(url, headers=user, allow_redirects=False)
-    if response.status_code == 404:
+    if not response.ok:
         return None
     data = response.json().get('data')
     after = data.get('after')
     for post in data.get('children'):
-        if post.get('data').get('title') in hot_list:
-            return hot_list
         hot_list.append(post.get('data').get('title'))
+    if after is None:
+        return hot_list
     return recurse(subreddit, hot_list, after)
