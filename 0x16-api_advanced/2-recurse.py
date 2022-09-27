468fc68c1a4ea5f3a526519c779e2b5bdbb35e6d
adut24 <dutertre.alexandre@laposte.net>
2022-09-27 14:57:42 +0200

    Remove fstrings

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,9 +5,9 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=''):
     """List all title in hot of a subreddit"""
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     if len(after) > 0:
-        url += f'?after={after}'
+        url += '?after={}'.format(after)
     user = {'User-Agent': 'Test123'}
     response = requests.get(url, headers=user, allow_redirects=False)
     if not response.ok:
