9d71fa0c726d63a3e5fcba3e9440e32e0d57918c
adut24 <dutertre.alexandre@laposte.net>
2022-09-28 13:54:33 +0200

    Correction

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,9 +6,6 @@ import requests
 
 def count_words(subreddit, word_list, after='', count={}):
     """Check the how recurring a list of words are in subreddit title"""
-    if count == {}:
-        for word in word_list:
-            count[word] = 0
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     if after != '':
         url += '?after={}'.format(after)
@@ -19,10 +16,13 @@ def count_words(subreddit, word_list, after='', count={}):
     data = response.json().get('data')
     after = data.get('after')
     for post in data.get('children'):
-        for word in post.get('data').get('title').split():
-            for keyword in count.keys():
-                if keyword.casefold() == word.casefold():
-                    count[keyword] += 1
+        for word in post.get('data').get('title').casefold().split():
+            for keyword in word_list:
+                if keyword.casefold() == word:
+                    if keyword.casefold() not in count.keys():
+                        count[word] = 1
+                    else:
+                        count[word] += 1
     if after is None:
         sorted_count = dict(sorted(count.items(), key=operator.itemgetter(1),
                                    reverse=True))
